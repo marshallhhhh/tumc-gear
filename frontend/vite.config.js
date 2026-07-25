19c9791096f651a5793ac70efb5383e7a08ee4f6
Marshall Hodges <marshall.hodges.00@gmail.com>
2026-07-25 13:15:29 +1000

    added tag size feature

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
